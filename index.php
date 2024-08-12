cf441f4343a84346fd3b120388a91ffb123703bf
Mahnoor Tabassum <163853352+Mahnoor760@users.noreply.github.com>
2024-08-12 18:02:45 +0500

    php project

--- index.php
+++ index.php
@@ -1,181 +1,232 @@
 <?php
-
 $server = "localhost";
 $username = "root";
 $password = "";
-$database = "noor";
-
-
+$database = "aptech";
 
 $con = mysqli_connect($server, $username, $password, $database);
-
-
-
-
 if (!$con) {
-    die("connection failed hai " . mysqli_connect_error());
+    die("connection is failed" . mysqli_connect_error());
 }
-
-
 
 
 
 if (isset($_POST["submit"])) {
-   
-    $fname = $_POST["firstname"];
-    $lname = $_POST["lastname"];
+    $fname = $_POST["fname"];
     $email = $_POST["email"];
-    $phone = $_POST["phone"];
+    $password= $_POST["password"];
+    $conpassword = $_POST["conpassword"];
+    $gender = $_POST["gender"];
+    $dob = $_POST["dob"];
+    $address = $_POST["address"];
+    
+    $sqliinsert = "INSERT INTO `pinfo`(`fname`, `email`, `password`, `conpassword`, `gender`,`dob`,`address`) VALUES ('$fname', '$email','$password', '$conpassword','$gender', '$dob','$address')";
+    $res = mysqli_query($con, $sqliinsert);
 
-    $sqlins = "INSERT INTO `data`(`id`, `firstname`, `lastname`, `EmailAddress`, `phone`) VALUES ('$id','$firstname','$lastname','$EmailAddress','$phone')";
-
-
-    $result = mysqli_query($con, $sqlins);
-    if ($result) {
-        echo "Data Inserted";
+    if ($res) {
+        echo "inserted";
     }
 }
+
+
+else if (isset($_POST["update"])) {
+    $fname = $_POST["fnameupdate"];
+    $email = $_POST["emailupdate"];
+    $password = $_POST["passwordupdate"];
+    $conpassword = $_POST["conpasswordupdate"];
+    $gender = $_POST["genderupdate"];
+    $dob = $_POST["dobupdate"];
+    $address = $_POST["addressupdate"];
+    $update = "UPDATE `pinfo` SET `fname`='$fname' ,`email`='$email',`password`='$password', `conpassword`='$conpassword',`gender`='$gender',`dob`='$dob',`address`='$address' WHERE '";
+    $result = mysqli_query($con, $update);
+
+    if ($result) {
+        echo "updated";
+    }
+}
+    
 
 
 
 
 ?>
 
-<h1><center>PHP CRUD</center></h1>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+ 
 
 </head>
+
 <body>
+    <!-- Button trigger modal -->
+    <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+    </button> -->
+
+    <!-- Modal -->
     
-<div class="container">
-
-
-
-        <form action="" method="post">
-
-
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">First name</label>
-                <input type="text" class="form-control" name="first name" id="exampleFormControlInput1" placeholder="Enter your first name">
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" style="background-color:grey";>
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Update Your Records</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                
+                <div class="modal-body">
+                    <form action="index.php" method="post"  style="border: 2px solid black; padding: 15px; border-radius: 5px;">
+                        <input type="hidden" name="idupdate" id="userid">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">First Name</label>
+                            <input type="fname" name="fnameupdate" class="form-control" id="fnameV" placeholder="enter your full name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Email</label>
+                            <input type="email" name="emailupdate" class="form-control" id="emailV" placeholder="name@example.com">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Password</label>
+                            <input type="password" name="passwordupdate" class="form-control" id="passwordV" placeholder="enter your password">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Confirm Password</label>
+                            <input type="conpassword" name="conpasswordupdate" class="form-control" id="conpasswordV" placeholder="confirm your password">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Gender</label>
+                        </div>
+                        <div>  <input type="radio" id="Male" name="drone" value="Male" checked />
+            <label>Male</label>
+             </div>
+            <div>  <input type="radio" id="Female" name="drone" value="Female" />
+            <label>Female</label>
             </div>
-            <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Last name</label>
-                <textarea class="form-control" name="lastname" id="exampleFormControlTextarea1" placeholder="Enter your last name" ></textarea>
+            <div>  <input type="radio" id="Others" name="drone" value="Others" />
+            <label>Others</label>
             </div>
-            <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Email</label>
-                <textarea class="form-control" name="email" id="exampleFormControlTextarea1" placeholder="Enter your email" ></textarea>
+            </fieldset><br>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">date of birth</label>
+                            <input type="dob" name="dobupdate" class="form-control" id="dobV" placeholder="dd/mm/yy">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlTextarea1" class="form-label">Address</label>
+                            <textarea class="form-control" name="addressupdate" id="addressV" rows="3" placeholder="enter your address" ></textarea>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </form>
+
+                </div>
             </div>
+        </div>
+    </div>
+    <h2><center>REGISTRATION FORM</center></h2>
+    <div class="container">
+        <form action="index.php" method="post" style="font-size:20px; width: 60%; margin: 0 auto; border: 2px solid black; padding: 15px; border-radius: 5px;" >
+            
             <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Phone No</label>
-                <textarea class="form-control" name="phone" id="exampleFormControlTextarea1" placeholder="Enter your phone number" ></textarea>
+                            <label for="exampleFormControlInput1" class="form-label">Full Name</label>
+                            <input type="fname" name="fnameupdate" class="form-control" id="fnameV" placeholder="enter your full name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Email</label>
+                            <input type="email" name="emailupdate" class="form-control" id="emailV" placeholder="name@example.com">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Password</label>
+                            <input type="password" name="passwordupdate" class="form-control" id="passwordV" placeholder="enter your password">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Confirm Password</label>
+                            <input type="conpassword" name="conpasswordupdate" class="form-control" id="conpasswordV" placeholder="confirm your password">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Gender</label>    
+                        </div>
+                        <div>  <input type="radio" id="Male" name="drone" value="Male" checked />
+            <label>Male</label>
+             </div>
+            <div>  <input type="radio" id="Female" name="drone" value="Female" />
+            <label>Female</label>
             </div>
+            <div>  <input type="radio" id="Others" name="drone" value="Others" />
+            <label>Others</label>
+            </div>
+            </fieldset><br>
 
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Date of Birth</label>
+                            <input type="dob" name="dobupdate" class="form-control" id="dobV" placeholder="dd/mm/yy">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlTextarea1" class="form-label">Address</label>
+                            <textarea class="form-control" name="addressupdate" id="addressV" rows="3" placeholder="enter your address"></textarea>
+                        </div>
+                        <button class="btn btn-success" type="submit" style="width: 105px; height: 50px; font-size: 20px; padding: 10px 20px;" >Submit</button>
 
-
-
-            <button type="submit" class="btn btn-warning"> Submit </button>
         </form>
-
-<br><br>
-
-
-
-        <h1>
-           Your Data will insert here!!
-        </h1>
-
-
-        <table class="table table-striped table-hover">
+    </div>
+    <br>
+<br>
+<h2><center>All Records </center></h2>
+<div class="container">
+        <table border="7" class="table" >
             <thead>
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First name</th>
-                    <th scope="col">Last name</th>
+                    <th scope="col">First Name</th>
                     <th scope="col">Email</th>
-                    <th scope="col">Phone No</th>
+                    <th scope="col">Password</th>
+                    <th scope="col">Confirm Password</th>
+                    <th scope="col">Gender</th>
+                    <th scope="col">Date of Birth</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Handle</th>
                 </tr>
-               
-                
             </thead>
             <tbody>
-
-
                 <?php
-
-                $select = "Select * from data ";
-              
-
-                $listresult = mysqli_query($con, $select); 
-
-                while ($row = mysqli_fetch_assoc($listresult))    ?> 
-                    
-                
-
-
-                <th scope="row">     </th>
-                <td scope="row">    </td>
-                <td scope="row">    </td>
-                <td scope="row">    </td>
-                <td scope="row">    </td>
-         
-                <td>
-                    <button type="button" class="btn btn-primary">Edit</button>
-                    <button type="button" class="btn btn-secondary">Delete</button></td>
-                 </tr> 
-            
-                    
-          
-                     
-                
-
                
-
-
-
             
+                $sql = "SELECT * FROM `pinfo`";
+                $result = mysqli_query($con, $sql);
+                
+                while ($a = mysqli_fetch_assoc($result)) {
+                    echo "<tr>";
 
-
+                    echo "<td scope='row'>" . $a["fname"] . "</td>";
+                    echo "<td scope='row'>" . $a["email"] . "</td>";
+                    echo "<td scope='row'>" . $a["password"] . "</td>";
+                    echo "<td scope='row'>" . $a["conpassword"] . "</td>";
+                    echo "<td scope='row'>" . $a["gender"] . "</td>";
+                    echo "<td scope='row'>" . $a["dob"] . "</td>";
+                    echo "<td scope='row'>" . $a["address"] . "</td>";   
+                    echo "<td scope='row'>  
+                        <div class='d-grid gap-2 d-md-block'>
+                            <button class='btn btn-primary edit' data-bs-toggle='modal' data-bs-target='#exampleModal' type='button' id=" . ">Edit</button>
+                            <button class='btn btn-danger delete' type='button' id="  .   ">Delete</button>
+                        </div>
+                    </td>";
+                    echo "</tr>";
+                }
+                ?>
             </tbody>
         </table>
-
-
-
-
-
-
-
-
-
-
     </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
+   
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-
-
-
-
-
+   
 </body>
+
 </html>
+```
